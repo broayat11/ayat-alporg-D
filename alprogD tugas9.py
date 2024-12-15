f907b8c6caf7a341a5e2039dda52f7242e45acc8
broayat11 <hijratulhidayat@gmail.com>
2024-12-15 14:40:22 +0800

    Add files via upload

---
+++ alprogD tugas9.py
@@ -0,0 +1,12 @@
+celcius = 46
+celcius = int(input('masukan suhu dalam celcius'))
+print(nilai suhu adalah,celcius)
+#reamur
+reamur = (45)  celcius
+print(nilai suhu adalah,reamur,reamur)
+#fahrenheit
+fahrenheit = ((95)  celcius) + 32
+print(nilai suhu adalah,fahrenheit,fahrenheit)
+#kelvin
+kelvin = celcius + 273
+print(nilai suhu adalah,kelvin,kelvin)
